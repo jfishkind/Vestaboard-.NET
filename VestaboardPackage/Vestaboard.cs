ec3bf9a78adb19eae12695f862538526dbcd67a9
jfishkind <jake.fishkind@gmail.com>
2024-05-09 20:45:05 -0400

    Initial commit

---
+++ VestaboardPackage/Vestaboard.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace VestaboardPackage;
+
+public class VestaboardClient
+{
+    private string _key { get; }
+
+    public VestaboardClient()
+    {
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<VestaboardClient>()
+            .Build();
+
+        _key = config["READ_WRITE_KEY"];
+    }
+}
