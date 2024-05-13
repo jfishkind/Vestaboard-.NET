79c01c1c23ca97d296fd795e73f8b787babb5223
jfishkind <jake.fishkind@gmail.com>
2024-05-13 17:27:39 -0400

    pls

---
+++ Sandbox/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+// Make sure to add user secrets before running this file
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+    
+    
